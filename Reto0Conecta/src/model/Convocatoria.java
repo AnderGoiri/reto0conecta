264369fd9e0ea3aa804da792e6e1f8f66ea67b63
sunder-012 <jagoba1207@gmail.com>
2023-09-15 09:05:06 +0200

    Finished model
    Model classes' attributes done.

---
+++ Reto0Conecta/src/model/Convocatoria.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author 2dam
+ */
+public class Convocatoria {
+    private String convocatoria;
+    private String descripcion;
+    private LocalDate fecha;
+    private String curso;
+
+    public String getConvocatoria() {
+        return convocatoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setConvocatoria(String convocatoria) {
+        this.convocatoria = convocatoria;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public Convocatoria(String convocatoria, String descripcion, LocalDate fecha, String curso) {
+        this.convocatoria = convocatoria;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.curso = curso;
+    }
+    
+}
---
+++ Reto0Conecta/src/model/Enunciado.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author 2dam
+ */
+public class Enunciado {
+    private int id;
+    private String descripcion;
+    private Dificultad nivel;
+    private boolean disponible;
+    private String ruta;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public Dificultad getNivel() {
+        return nivel;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setNivel(Dificultad nivel) {
+        this.nivel = nivel;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public Enunciado(int id, String descripcion, Dificultad nivel, boolean disponible, String ruta) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.nivel = nivel;
+        this.disponible = disponible;
+        this.ruta = ruta;
+    }
+    
+}
