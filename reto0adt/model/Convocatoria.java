8ad57058085a13ddb272b25fee8aba46b71edc04
Andoni-S <andonisanzalcalde@gmail.com>
2023-09-15 09:31:23 +0200

    PDF visualize test

--- reto0adt/model/Convocatoria.java
+++ reto0adt/model/Convocatoria.java
@@ -3,12 +3,57 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package reto0adt.model;
+package model;
+
+import java.time.LocalDate;
 
 /**
  *
  * @author 2dam
  */
 public class Convocatoria {
+    private String convocatoria;
+    private String descripcion;
+    private LocalDate fecha;
+    private String curso;
+
+    public String getConvocatoria() {
+        return convocatoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setConvocatoria(String convocatoria) {
+        this.convocatoria = convocatoria;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public Convocatoria(String convocatoria, String descripcion, LocalDate fecha, String curso) {
+        this.convocatoria = convocatoria;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.curso = curso;
+    }
     
 }
--- reto0adt/model/Dificultad.java
+++ reto0adt/model/Dificultad.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package reto0adt.model;
+package model;
 
 /**
  *
  * @author 2dam
  */
 public enum Dificultad {
-    
+    ALTA, MEDIA,BAJA;
 }
